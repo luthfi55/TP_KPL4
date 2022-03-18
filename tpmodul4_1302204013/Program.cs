0fbc6aa3ca617399afddcc508f823d984f6bb4da
luthfi55 <luthfi12354@gmail.com>
2022-03-18 16:06:15 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302204013/Program.cs
+++ tpmodul4_1302204013/Program.cs
@@ -6,7 +6,35 @@ namespace tpmodul4_1302204013
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+            Console.WriteLine("Masukkan Nama User");
+            String x = Console.ReadLine();
+            halo.SapaUser(x);
+            Console.WriteLine("\nMasukkan Data");
+            String Data = Console.ReadLine();
+            DataGeneric<String> data = new DataGeneric<string>(Data);
+            data.PrintData();
+        }
+    }
+
+    public class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("\nHalo user " + x);
+        }
+    }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("\nData yang tersimpan adalah: " + this.data);
         }
     }
 }
+
